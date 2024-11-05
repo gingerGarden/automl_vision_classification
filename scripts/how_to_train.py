23707be2883eb46ccb21433ca41bb7ff56a53736
gingerGarden <gooopylim@gmail.com>
2024-11-05 16:45:19 +0900

    first commit

---
+++ scripts/how_to_train.py
@@ -0,0 +1,18 @@
+# seed
+# ========================================
+SEED = 1234
+
+
+# idx_dict 방식
+# ========================================
+K_SIZE = 5
+TEST_RATIO = 0.2
+VALID_RATIO = 0.1
+
+
+# 경로 정보
+# ========================================
+SOURCE = "source"
+LOG = "log"
+RESULT = "result"
+ESPOINT_DIR = "estop_point"
